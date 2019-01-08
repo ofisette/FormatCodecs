e2d5f12ad7f0f31ea0ccaf969ebaa576d4b1dfa3
Olivier Fisette <ofisette@gmail.com>
2019-01-08 15:53:50 +0100

    Register codecs at runtime in the init function.

--- src/FormatCodecs.jl
+++ src/FormatCodecs.jl
@@ -1,33 +1,30 @@
-module FormatCodecs
-
-using Formats
-
-using CodecZlib
-
-Formats.addcoding("application/gzip")
-Formats.addextension("application/gzip", ".gz")
-Formats.addextension("application/gzip", ".gzip")
-Formats.setdecoder("application/gzip", GzipDecompressor)
-Formats.setencoder("application/gzip", GzipCompressor)
-
-using CodecBzip2
-
-Formats.addcoding("application/x-bzip2")
-Formats.addextension("application/x-bzip2", ".bz2")
-Formats.addextension("application/x-bzip2", ".bzip2")
-Formats.setdecoder("application/x-bzip2", Bzip2Decompressor)
-Formats.setencoder("application/x-bzip2", Bzip2Compressor)
-
-using CodecXz
-
-Formats.addcoding("application/x-xz")
-Formats.addextension("application/x-xz", ".xz")
-Formats.setdecoder("application/x-xz", XzDecompressor)
-Formats.setencoder("application/x-xz", XzCompressor)
-
 """
 Integrate common transcoders with Formats.
 """
-FormatCodecs
+module FormatCodecs
+
+using Formats
+using CodecZlib
+using CodecBzip2
+using CodecXz
+
+function __init__()
+	Formats.addcoding("application/gzip")
+	Formats.addextension("application/gzip", ".gz")
+	Formats.addextension("application/gzip", ".gzip")
+	Formats.setdecoder("application/gzip", GzipDecompressor)
+	Formats.setencoder("application/gzip", GzipCompressor)
+
+	Formats.addcoding("application/x-bzip2")
+	Formats.addextension("application/x-bzip2", ".bz2")
+	Formats.addextension("application/x-bzip2", ".bzip2")
+	Formats.setdecoder("application/x-bzip2", Bzip2Decompressor)
+	Formats.setencoder("application/x-bzip2", Bzip2Compressor)
+
+	Formats.addcoding("application/x-xz")
+	Formats.addextension("application/x-xz", ".xz")
+	Formats.setdecoder("application/x-xz", XzDecompressor)
+	Formats.setencoder("application/x-xz", XzCompressor)
+end
 
 end # module
